52cd4eff60d3b589a38467c77af1d4c52e9a44dc
ttppss <zgjxxzn@gmail.com>
2020-05-17 15:45:45 -0400

    print out results bboxes

--- tools/metric_polyp.py
+++ tools/metric_polyp.py
@@ -2,31 +2,32 @@ import numpy as np
 import cv2
 import os
 import matplotlib.pyplot as plt
-#from scipy.interpolate import spline
-#from scipy.interpolate import make_interp_spline, BSpline
+# from scipy.interpolate import spline
+# from scipy.interpolate import make_interp_spline, BSpline
 from matplotlib import colors
+
 
 # import json
 class Metric(object):
-    def __init__(self, mode='center',iou_thresh=0,visualize = True,visualization_root='/data1/zinan_xiong/'):
+    def __init__(self, mode='center', iou_thresh=0, visualize=True, visualization_root='/data1/zinan_xiong/'):
 
         self.TPs = []
         self.FNs = []
         self.FPs = []
         self.pred_bbox_count = 0
-        assert mode == 'center' or mode =='iou' , '({}) mode is not supported'
+        assert mode == 'center' or mode == 'iou', '({}) mode is not supported'
         self.mode = mode
-        self.iou_thresh=iou_thresh
+        self.iou_thresh = iou_thresh
 
-        #in BGR order
+        # in BGR order
         # Blue
-        self.FP_color= (255, 0, 0)
+        self.FP_color = (255, 0, 0)
         # Green
         self.Detection_color = (0, 255, 0)
         # Red
         self.GT_color = (0, 0, 255)
-        self.visualize=visualize
-        self.total_gt =0.0
+        self.visualize = visualize
+        self.total_gt = 0.0
         if visualize:
             #  create image folder for saving detection result
             self.detection_folder = visualization_root + 'ALL/'
@@ -39,17 +40,16 @@ class Metric(object):
             os.popen('rm -r ' + self.false_positive_folder + '*')
             os.popen('rm -r ' + self.false_negative_folder + '*')
 
-    def eval_add_result(self, ground_truth:list, pred_points:list, image:np.ndarray = None, image_name= None):
+    def eval_add_result(self, ground_truth: list, pred_points: list, image: np.ndarray = None, image_name=None):
         if self.visualize:
             FPimage = image.copy()
             FNimage = image.copy()
             Detectionimage = image.copy()
 
-
             for pt in pred_points:
                 pt1 = tuple([int(pt[0]), int(pt[1])])
                 pt2 = tuple([int(pt[2]), int(pt[3])])
-                cv2.rectangle(Detectionimage, pt1, pt2,self.Detection_color, 2)
+                cv2.rectangle(Detectionimage, pt1, pt2, self.Detection_color, 2)
 
         missing = False
         self.total_gt += len(ground_truth)
@@ -64,23 +64,23 @@ class Metric(object):
                 for j in pred_point:
                     # j = [j[0], j[1], j[2], j[3]]
                     self.pred_bbox_count += 1
-                    print("pred_boxes type: {}".format([type(i) for i in j], "\n"))
-                    print("j0, j1, j2, j3", j[0], j[1], j[2], j[3])
+                    # print("pred_boxes type: {}".format([type(i) for i in j], "\n"))
+                    # print("j0, j1, j2, j3", j[0], j[1], j[2], j[3])
                     if self.mode == 'center':
                         ctx = j[0] + (j[2] - j[0]) * 0.5
                         cty = j[1] + (j[3] - j[1]) * 0.5
-                        bbox_matched=gt[0] < ctx < gt[2] and gt[1] < cty < gt[3]
+                        bbox_matched = gt[0] < ctx < gt[2] and gt[1] < cty < gt[3]
 
-                    elif self.mode =='iou':
-                        query_area =(j[2] - j[0])*(j[3] - j[1])
-                        gt_area = (gt[2] - gt[0])*(gt[3] - gt[1])
-                        iw = (min(j[2],gt[2])-max(j[0],gt[0]))
-                        ih =(min(j[3],gt[3])-max(j[1],gt[1]))
-                        iw =max(0, iw)
+                    elif self.mode == 'iou':
+                        query_area = (j[2] - j[0]) * (j[3] - j[1])
+                        gt_area = (gt[2] - gt[0]) * (gt[3] - gt[1])
+                        iw = (min(j[2], gt[2]) - max(j[0], gt[0]))
+                        ih = (min(j[3], gt[3]) - max(j[1], gt[1]))
+                        iw = max(0, iw)
                         ih = max(0, ih)
-                        ua = query_area+gt_area -(iw*ih)
-                        overlaps = (iw*ih)/float(ua)
-                        bbox_matched= overlaps>self.iou_thresh
+                        ua = query_area + gt_area - (iw * ih)
+                        overlaps = (iw * ih) / float(ua)
+                        bbox_matched = overlaps > self.iou_thresh
 
                     if bbox_matched:
                         TP_Count += 1
@@ -95,8 +95,8 @@ class Metric(object):
             # if TP_Count > 0:
             #     hasTP = True
 
-            #pred_points = not_matched
-            #self.FPs += len(not_matched)
+            # pred_points = not_matched
+            # self.FPs += len(not_matched)
 
             if not hasTP:
                 self.FNs.append(gt)
@@ -107,7 +107,6 @@ class Metric(object):
                     pt1 = tuple([int(gt[0]), int(gt[1])])
                     pt2 = tuple([int(gt[2]), int(gt[3])])
                     cv2.rectangle(FNimage, pt1, pt2, self.GT_color, 2)
-
 
             if self.visualize:
                 # Draw groundturth on detection and FP images
@@ -123,9 +122,9 @@ class Metric(object):
 
         if self.visualize:
             if missing:
-                cv2.imwrite(self.false_negative_folder+str(image_name)+'.jpg', FNimage)
+                cv2.imwrite(self.false_negative_folder + str(image_name) + '.jpg', FNimage)
             cv2.imwrite(self.detection_folder + str(image_name) + '.jpg', Detectionimage)
-        if len(pred_points)>0 and self.visualize:
+        if len(pred_points) > 0 and self.visualize:
             # Draw false positive rect
             for fp in pred_points:
                 pt1 = tuple([int(fp[0]), int(fp[1])])
@@ -134,20 +133,19 @@ class Metric(object):
             cv2.imwrite(self.false_positive_folder + str(image_name) + '.jpg', FPimage)
         #  add FP here
         self.FPs += pred_points
-        
 
     def get_result(self):
-        if float(len(self.TPs) + len(self.FPs))==0:
-            precision=0
+        if float(len(self.TPs) + len(self.FPs)) == 0:
+            precision = 0
         else:
             precision = float(len(self.TPs)) / float(len(self.TPs) + len(self.FPs))
-            #print("length of TPs", len(self.TPs), "\n")
-            #print("length of FPs", len(self.TPs), "\n")
-            #print("length of FNs", len(self.FNs), "\n")
-        if float (len(self.TPs) + len(self.FNs))==0:
-            recall=0
+            # print("length of TPs", len(self.TPs), "\n")
+            # print("length of FPs", len(self.TPs), "\n")
+            # print("length of FNs", len(self.FNs), "\n")
+        if float(len(self.TPs) + len(self.FNs)) == 0:
+            recall = 0
         else:
-            recall = float(len(self.TPs)) /float (len(self.TPs) + len(self.FNs))
+            recall = float(len(self.TPs)) / float(len(self.TPs) + len(self.FNs))
         return precision, recall, self.pred_bbox_count
 
     def reset(self):
@@ -157,39 +155,37 @@ class Metric(object):
         self.total_gt = 0.0
 
     def get_tpr_fpr(self):
-        return len(self.TPs)/self.total_gt,len(self.FPs)/self.total_gt
+        return len(self.TPs) / self.total_gt, len(self.FPs) / self.total_gt
 
 
 class Result_writer(object):
-    def __init__(self,path):
-
+    def __init__(self, path):
 
         self.myfile = open(path, "a")
 
-    def append_detection(self,predection:list,groud_truth:list,image_name):
+    def append_detection(self, predection: list, groud_truth: list, image_name):
         self.myfile.write('image {} \n'.format(image_name))
         self.myfile.write('predection# {} \n'.format(len(predection)))
         for i in predection:
-            self.myfile.write('{} {} {} {} {} \n'.format(i[0],i[1],i[2],i[3],i[4]))
+            self.myfile.write('{} {} {} {} {} \n'.format(i[0], i[1], i[2], i[3], i[4]))
         self.myfile.write('ground_truth# {} \n'.format(len(groud_truth)))
         for i in groud_truth:
-            self.myfile.write('{} {} {} {} \n'.format(i[0],i[1],i[2],i[3]))
+            self.myfile.write('{} {} {} {} \n'.format(i[0], i[1], i[2], i[3]))
 
 
 class Result_writer_multi_class(object):
-    def __init__(self,path):
-
+    def __init__(self, path):
 
         self.myfile = open(path, "a")
 
-    def append_detection(self,predection:list,groud_truth:list,image_name):
+    def append_detection(self, predection: list, groud_truth: list, image_name):
         self.myfile.write('image {} \n'.format(image_name))
         self.myfile.write('predection# {} \n'.format(len(predection)))
         for i in predection:
-            self.myfile.write('{} {} {} {} {} {} \n'.format(i[0],i[1],i[2],i[3],i[4],i[5]))
+            self.myfile.write('{} {} {} {} {} {} \n'.format(i[0], i[1], i[2], i[3], i[4], i[5]))
         self.myfile.write('ground_truth# {} \n'.format(len(groud_truth)))
         for i in groud_truth:
-            self.myfile.write('{} {} {} {} {} \n'.format(i[0],i[1],i[2],i[3],i[4]))
+            self.myfile.write('{} {} {} {} {} \n'.format(i[0], i[1], i[2], i[3], i[4]))
 
 
 class Result_plot(object):
@@ -200,7 +196,7 @@ class Result_plot(object):
         self.target_lists = []
         self.score_list = []
 
-        self.file_name =os.path.splitext(os.path.basename(path))[0]
+        self.file_name = os.path.splitext(os.path.basename(path))[0]
         self.file_dir = os.path.dirname(path)
         with open(path, 'r') as myfile:
             results = myfile.readlines()
@@ -208,20 +204,20 @@ class Result_plot(object):
         while count < len(results):
             count += 1
             n_predicted = int(results[count].split(' ')[1])
-            pred_list=[]
+            pred_list = []
             for i in range(n_predicted):
                 count += 1
                 bbox = results[count].rstrip().split(' ')
                 bbox = np.array([float(i) for i in bbox])
-                score =bbox[0]
-                if score!=0:
+                score = bbox[0]
+                if score != 0:
                     self.score_list.append(score)
                 pred_list.append(bbox)
             self.pred_lists.append(np.vstack(pred_list))
             count += 1
             n_ground = int(results[count].split(' ')[1])
 
-            target_list=[]
+            target_list = []
             for i in range(n_ground):
                 count += 1
                 bbox = results[count].rstrip().split(' ')[0:4]
@@ -236,23 +232,22 @@ class Result_plot(object):
         for thresh in threshs:
             evals = Metric(visualize=False, mode='center', visualization_root='demo/{:0.3f}/'.format(thresh))
             for index in range(len(self.pred_lists)):
-                pred_list=self.pred_lists[index]
-                pred_list=pred_list[pred_list[:,0]>thresh][:,1:]
-                evals.eval_add_result(self.target_lists[index],pred_list)
+                pred_list = self.pred_lists[index]
+                pred_list = pred_list[pred_list[:, 0] > thresh][:, 1:]
+                evals.eval_add_result(self.target_lists[index], pred_list)
 
             precision, recall = evals.get_result()
-            F1 = 2*(precision*recall)/(precision+recall)
-            F2= 5*(precision*recall)/(4*precision+recall)
+            F1 = 2 * (precision * recall) / (precision + recall)
+            F2 = 5 * (precision * recall) / (4 * precision + recall)
             self.precisions.append(precision)
             self.recalls.append(recall)
-            out ="precision: {:.4f}  recall:  {:.4f} F1: {:.4f} F2: {:.4f} thresh: {:.4f} TP: {} FP: {} FN: {}"\
-                .format(precision,recall,F1,F2,thresh,len(evals.TPs),len(evals.FPs),len(evals.FNs))
+            out = "precision: {:.4f}  recall:  {:.4f} F1: {:.4f} F2: {:.4f} thresh: {:.4f} TP: {} FP: {} FN: {}" \
+                .format(precision, recall, F1, F2, thresh, len(evals.TPs), len(evals.FPs), len(evals.FNs))
             print(out)
-
 
     def get_plot(self):
 
-        precisions,recalls = self.get_pr()
+        precisions, recalls = self.get_pr()
 
         fig = plt.figure(figsize=(5, 5))
         ax = fig.add_subplot(1, 1, 1)
@@ -265,8 +260,6 @@ class Result_plot(object):
         plt.tight_layout()
         plt.savefig(self.file_dir + '/{}.jpg'.format(self.file_name, dpi=fig.dpi, pad_inches=10))
         plt.show()
-
-
 
     def get_pr(self):
         precisions = []
@@ -342,7 +335,6 @@ class Result_plot(object):
             ax.grid()
             ax.set(xlabel='box size', ylabel='number of boxes')
 
-
             plt.tight_layout()
 
             # plt.savefig(save_location, dpi=fig.dpi, pad_inches=10)
@@ -354,11 +346,11 @@ def plot_best():
         fig = plt.figure(figsize=(4.5, 3.75))
         ax = fig.add_subplot(1, 1, 1)
         file_dic = {
-            'resnet-50':'/home/dwang/data0/SynologyDrive/ssd.pytorch-master/aws_result/resnet_50_wrong_gaussian/95.txt',
+            'resnet-50': '/home/dwang/data0/SynologyDrive/ssd.pytorch-master/aws_result/resnet_50_wrong_gaussian/95.txt',
             'resnet-101': '/home/dwang/data0/SynologyDrive/ssd.pytorch-master/aws_result/resnet_101_wrong_gaussian/25.txt',
-            'SSD_baseline':'/home/dwang/data0/SynologyDrive/ssd.pytorch-master/aws_result/SSD_baseline/65.txt',
-            'Ours:VGG16':'/home/dwang/data0/SynologyDrive/ssd.pytorch-master/aws_result/VGG16_COSINE_GASSUIAN_NO_FPN_FEM/75.txt',
-            'Ours:VGG16+FPN':'/home/dwang/data0/SynologyDrive/ssd.pytorch-master/aws_result/VGG16_FPN_COSINE_GASSUIAN_NO_FEM/55.txt',
+            'SSD_baseline': '/home/dwang/data0/SynologyDrive/ssd.pytorch-master/aws_result/SSD_baseline/65.txt',
+            'Ours:VGG16': '/home/dwang/data0/SynologyDrive/ssd.pytorch-master/aws_result/VGG16_COSINE_GASSUIAN_NO_FPN_FEM/75.txt',
+            'Ours:VGG16+FPN': '/home/dwang/data0/SynologyDrive/ssd.pytorch-master/aws_result/VGG16_FPN_COSINE_GASSUIAN_NO_FEM/55.txt',
             'Ours:VGG16+FPN+CEM': '/home/dwang/data0/SynologyDrive/ssd.pytorch-master/aws_result/0.txt'
         }
 
@@ -369,7 +361,7 @@ def plot_best():
 
             ax.plot(recalls, precisions)
 
-            precisions, recalls = np.array(precisions),np.array(recalls)
+            precisions, recalls = np.array(precisions), np.array(recalls)
 
             # sorted_index = precisions.argsort()
             # precisions = precisions[sorted_index]
@@ -389,6 +381,7 @@ def plot_best():
         plt.savefig('aws_result' + '/{}.pdf'.format('best', dpi=fig.dpi, pad_inches=10))
         plt.show()
 
+
 if __name__ == '__main__':
     # for i in range(10,80,5):
     #     print('epoc:{}'.format(i))
@@ -404,13 +397,7 @@ if __name__ == '__main__':
     # r = Result_plot('aws_result/correct_gaussian_alpha_1/{}.txt'.format(65))
     # r.get_result_scale()
 
-
-
-
-
     plot_best()
-
-
 
 # def pf():
 # dataset = Polyp('test')
@@ -453,9 +440,3 @@ if __name__ == '__main__':
 #
 #
 #
-
-
-
-
-
-
