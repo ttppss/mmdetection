80ad04384af86aefb2794e4a7206ffa0dcf8013d
ttppss <zgjxxzn@gmail.com>
2020-05-26 00:13:35 -0400

    modified _resize_bboxes in transforms.py

--- mmdet/datasets/pipelines/transforms.py
+++ mmdet/datasets/pipelines/transforms.py
@@ -150,8 +150,8 @@ class Resize(object):
             #     continue
             # else:
             # for bbox in results[key]:
-            # print('\n', 'results [key]: ', results[key])
-            # print('\n', 'scale factor: ', results['scale_factor'])
+            print('\n', 'results [key]: ', results[key])
+            print('\n', 'scale factor: ', results['scale_factor'])
             if results[key] is None:
                 continue
             bboxes = results[key] * results['scale_factor']
