78c520780748eb93860e01aeb7275fd5632de8b6
ttppss <zgjxxzn@gmail.com>
2020-05-25 23:46:49 -0400

    modified _resize_bboxes in transforms.py

--- mmdet/datasets/pipelines/transforms.py
+++ mmdet/datasets/pipelines/transforms.py
@@ -150,6 +150,8 @@ class Resize(object):
             #     continue
             # else:
             # for bbox in results[key]:
+            print('\n', 'results [key]: ', results[key])
+            print('\n', 'scale factor: ', results['scale_factor'])
             bboxes = results[key] * results['scale_factor']
             bboxes[:, 0::2] = np.clip(bboxes[:, 0::2], 0, img_shape[1])
             bboxes[:, 1::2] = np.clip(bboxes[:, 1::2], 0, img_shape[0])
