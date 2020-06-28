880dbfb37331f514ba849a4fa05370895984afb6
ttppss <zgjxxzn@gmail.com>
2020-06-28 18:06:06 -0400

    modified dist_test.sh, replaced with test_multiple_pickle

--- tools/dist_test.sh
+++ tools/dist_test.sh
@@ -7,4 +7,8 @@ PORT=${PORT:-29500}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-    $(dirname "$0")/test_multiple_pickle.py $CONFIG $CHECKPOINT --launcher pytorch ${@:4}
+    $(dirname "$0")/test_multiple_pickle.py $CONFIG --launcher pytorch ${@:4}
+
+#PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
+#python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
+#    $(dirname "$0")/test_multiple_pickle.py $CONFIG $CHECKPOINT --launcher pytorch ${@:4}
