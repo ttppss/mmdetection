3dec08c86322b36ef0595f0ff62915b97835490f
ttppss <zgjxxzn@gmail.com>
2020-07-02 12:15:26 -0400

    lr_config set to cosine

--- configs/_base_/schedules/schedule_1x.py
+++ configs/_base_/schedules/schedule_1x.py
@@ -3,10 +3,9 @@ optimizer = dict(type='SGD', lr=0.0002, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=None)
 # learning policy
 lr_config = dict(
-    policy='step',
+    policy='cosine',
     warmup='linear',
     warmup_iters=500,
-    warmup_ratio=0.001,
-    step=[8, 11])
+    warmup_ratio=1.0 / 3)
 # change the epoch from 12 to 6
 total_epochs = 12
